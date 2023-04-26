7d58e9a869c041969d17a2e2fbff80e26e4fa100
Bhuvan Kumar M <bhuvankumar3877@gmail.com>
2023-04-26 18:44:10 +0530

    Add files via upload

---
+++ LibraryApp/src/libapp/Main.java
@@ -0,0 +1,15 @@
+package libapp;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        LibraryApp app = new LibraryApp();
+        app.checkOutBook("83471");
+        app.checkOutBook("83471");
+        app.checkOutBook("83471");
+        app.checkOutBook("83471");
+        app.checkOutBook("83471");
+
+    }
+}
